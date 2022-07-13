6e0af131f32cd9a0eb06dfb4bfb9bb29fe092aef
ClydeRM <clyde.official@icloud.com>
2022-07-13 14:41:19 +0800

    feat(Main): Use ConfigService
    From ConfigModule
    Load src/common/config
    Setup App config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { PrismaService } from 'nestjs-prisma';
-import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
+import { AppModule } from 'src/app.module';
+import type { NestConfig } from 'src/common/config/config.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,14 @@ async function bootstrap() {
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  // Prisma Client Exception Filter for unhandled exceptions
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  // Use config module
+  const configService = app.get(ConfigService);
+  const nestConfig = configService.get<NestConfig>('nest');
+
+  await app.listen(process.env.PORT || nestConfig.port || 3000);
 }
 bootstrap();
