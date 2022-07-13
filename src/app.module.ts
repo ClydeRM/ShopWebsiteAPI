ada149318596e7a1adbd53deb37fb6d730977ff8
ClydeRM <clyde.official@icloud.com>
2022-07-13 14:40:07 +0800

    feat(AppModule): Import ConfigModule
    Load src/common/config
    Setup App config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'nestjs-prisma';
+import config from 'src/common/config/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from 'nestjs-prisma';
@@ -7,6 +10,7 @@ import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
     PrismaModule.forRoot({
       isGlobal: true,
       prismaServiceOptions: {
