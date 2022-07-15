1566bd7fa249413aa90b3114e89f7fc7e17406a2
ClydeRM <clyde.official@icloud.com>
2022-07-15 22:41:01 +0800

    feat(Strategy): AccessToken strategy
    Extends passportStrategy
    Implement validate function
    Return User object

---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from 'src/auth/auth.service';
+
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { JwtPayload } from 'src/auth/types/';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_ACCESS_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const user = await this.authService.validateUser(payload.userId);
+    if (!user) {
+      // User Not Found
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
