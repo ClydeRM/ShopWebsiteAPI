c9f60f319e02371143348057bc162304c48044d4
ClydeRM <clyde.official@icloud.com>
2022-07-15 22:49:38 +0800

    refactor(AuthController): Refactor route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,29 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninDto, SignupDto } from './dto';
+import { Tokens } from './types';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  @Post('/local/signup')
-  localSignup(@Body() signupDto: SignupDto) {
+  @Post('local/signup')
+  localSignup(@Body() signupDto: SignupDto): Promise<Tokens> {
     return this.authService.localSignup(signupDto);
   }
 
-  @Post('/local/signin')
-  localSignin(@Body() signinDto: SigninDto) {
+  @Post('local/signin')
+  localSignin(@Body() signinDto: SigninDto): Promise<Tokens> {
     return this.authService.localSignin(signinDto);
   }
 
   //   TODO: getCurrentUserId decorator
-  @Post('/logout')
+  @Post('logout')
   localLogout() {
     return this.authService.logout();
   }
 
   //   TODO: getCurrentUserId decorator
-  @Post('/refresh')
+  @Post('refresh')
   refresh() {
     return this.authService.refresh();
   }
