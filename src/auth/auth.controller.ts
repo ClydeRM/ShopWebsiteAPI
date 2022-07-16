f5f87b708064a663f2657abc3527f8cf8e7d46e7
ClydeRM <clyde.official@icloud.com>
2022-07-16 14:53:38 +0800

    feat(AuthController): Implement logout refresh
    Add Public() set API to Public
    Add RtGuard() to auth/refresh API
    RtGuard() use RtStrategy to validate Request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  GetCurrentUser,
+  GetCurrentUserId,
+  Public,
+} from 'src/common/decorators';
+import { RtGuard } from 'src/common/guards';
 import { AuthService } from './auth.service';
 import { SigninDto, SignupDto } from './dto';
 import { Tokens } from './types';
@@ -6,25 +19,35 @@ import { Tokens } from './types';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Public()
   @Post('local/signup')
+  @HttpCode(HttpStatus.CREATED)
   localSignup(@Body() signupDto: SignupDto): Promise<Tokens> {
     return this.authService.localSignup(signupDto);
   }
 
+  @Public()
   @Post('local/signin')
+  @HttpCode(HttpStatus.OK)
   localSignin(@Body() signinDto: SigninDto): Promise<Tokens> {
     return this.authService.localSignin(signinDto);
   }
 
-  //   TODO: getCurrentUserId decorator
   @Post('logout')
-  localLogout() {
-    return this.authService.logout();
+  @HttpCode(HttpStatus.OK)
+  logout(@GetCurrentUserId() userId: string) {
+    return this.authService.logout(userId);
   }
 
-  //   TODO: getCurrentUserId decorator
+  @Public()
+  @UseGuards(RtGuard)
   @Post('refresh')
-  refresh() {
-    return this.authService.refresh();
+  @HttpCode(HttpStatus.OK)
+  refresh(
+    @GetCurrentUserId() userId: string,
+    @GetCurrentUser('refreshToken') refresh_token: string,
+  ): Promise<Tokens> {
+    return this.authService.refreshToken(userId, refresh_token);
   }
 }
